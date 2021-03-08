dcda739f92cbfd4f5de15d6ac8f58e191ce4fecb
frantuma <frantuma@yahoo.com>
2021-03-08 22:05:45 +0100

    test-framework - build commands

--- .github/actions/generate/generate.sh
+++ .github/actions/generate/generate.sh
@@ -35,6 +35,7 @@ shift;
 
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -Dlogback.configurationFile=$SCRIPT/logback.xml"
 ags="generate -i ${SPEC_URL} -l ${LANG} -o generated/${JOB_NAME} $@"
+echo "AGS" $ags
 
 java $JAVA_OPTS -jar $executable $ags
 
